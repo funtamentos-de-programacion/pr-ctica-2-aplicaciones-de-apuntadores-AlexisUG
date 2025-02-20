f3342abc0a8dc9fade1f73e06242ecfc09250ab6
Guzman Castillo Alexis Uriel <kung.fu.xtream@gmail.com>
2025-02-20 10:55:22 -0600

    Archivos que se trabaron durante practica

--- ejercicio2.c
+++ ejercicio2.c
@@ -10,6 +10,7 @@ int main () {
   printf("Dirección del arreglo en la primera posición: %x\n",&arr[0]);
   // imprime la dirección de memoria del arreglo  (el nombre del arreglo es un apuntador)
   printf("Dirección del arreglo: %x\n",&arr);
+  printf("Dirección del arreglo: %x\n", arr);
   // imprime la dirección de memoria almacenada en el apuntador apArr
   printf("Dirección almacenada en el apuntador: %x\n",apArr);
 
@@ -22,16 +23,18 @@ int main () {
  
   printf("Recorriendo el arreglo usando un apuntador:\n");
   for (int i = 0; i < 5; i++) {
-    printf("Elemento %d: %d\n", i, *(ptr + i));
+    printf("Elemento %d: %d\n", i, *(apArr + i));
   }
 
   //Modifica apArr para que apunte al tercer elemento
-
+  apArr = &arr[3];
   //Accede al elemento con indice -2 usando el apuntador
+  printf("Elemento -2: %d\n", apArr[-2]);
+  printf("Elemento 0: %d\n", apArr[0]);
 
   //Ajusta el ciclo for para que se pueda recorrer completo el arreglo de acuerdo al elemento apuntado
-  for (int i = ??; i < ??; ???) {
-    printf("Elemento %d: %d\n", i, *(ptr + i));
+  for (int i = -3; i < 2; i++) {
+    printf("Elemento %d: %d\n", i, *(apArr + i));
   }
   return 0;
 }
